098a69160aa8219229d1c349e6a34e5c76b35776
graymalkin <sjc80@kent.ac.uk>
2014-11-07 15:35:25 +0000

    unit tests

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ThreadedIRCBot;
+using System.Collections.Generic;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class URLTests
+    {
+        [TestMethod]
+        public void GetTitle()
+        {
+            Assert.AreEqual(ThreadedIRCBot.URL.getTitle(@"http://www.google.com/"), "Google");
+        }
+
+        [TestMethod]
+        public void GetURL()
+        {
+            string[] k = {"http://www.flickr.com/photos/phaseviii/9217207106/in/set-72157626056708495"};
+            List<String> m = ThreadedIRCBot.URL.getURLs(k);
+
+            Assert.AreEqual(k[0], m[0]);
+        }
+    }
+}
